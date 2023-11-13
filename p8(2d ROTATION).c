0914622ad611dfe8ed93a02868712ca5d194644b
Adityamakwana <129506682+helloo707@users.noreply.github.com>
2023-11-13 13:57:15 +0530

    Create p8(2d ROTATION).c

--- p8(2d ROTATION).c
+++ p8(2d ROTATION).c
@@ -1,36 +1,39 @@
-#include <stdio.h>
-#include<graphics.h>
-#include<conio.h>
-#include<dos.h>
-int x1,y1,x2,y2;
-void draw(int ,int ,int, int);
-
-int main()
-{
-    int gd=DETECT,gm;
-    int degree,a,b,c,d,xp,yp;
-    initgraph(&gd,&gm,"C:\\tc\\bgi");
-    printf("\n ENTER X1,Y1 VALUES: ");
-    scanf("%d%d",&x1,&y1);
-    printf("\n ENTER X2,Y2 VALUES: ");
-    scanf("%d%d",&x2,&y2);
-    cleardevice();
-    draw(x1,y1,x2,y2);
-    setcolor(7);
-    printf("\n ENTER ANGLE FOR ROTATION: ");
-    scanf("%d",&degree);
-    a=cos((degree*3.14)/180);
-    b=sin((degree*3.14)/180);
-    c=(-sin((degree*3.14)/180));
-    d=(cos((degree*3.14)/180));
-    xp=((x2*a)-(y2*b));
-    yp=((x2*c)-(y2*d));
-    draw(x1,y1,xp,yp);
-    getch();
-    closegraph();
-    return 0;
-}
-
-void draw(int x1,int y1,int x2,int y2){
-    line(x1,y1,x2,y2);
-}
+#include<stdio.h>  
+#include<graphics.h>  
+#include<math.h>  
+main()  
+{  
+    int gd=DETECT,gm,x1,y1,x2,y2,x3,y3;  
+    double s,c, angle;  
+    initgraph(&gd, &gm, "C:\\tc\\bgi");  
+    setcolor(RED);  
+    printf("Enter coordinates of triangle: ");  
+    scanf("%d%d%d%d%d%d",&x1,&y1,&x2,&y2, &x3, &y3);  
+    setbkcolor(WHITE);  
+    cleardevice();  
+    line(x1,y1,x2,y2);  
+    line(x2,y2, x3,y3);  
+    line(x3, y3, x1, y1);  
+    getch();  
+    setbkcolor(BLACK);  
+    printf("Enter rotation angle: ");  
+    scanf("%lf", &angle);  
+    setbkcolor(WHITE);  
+    c = cos(angle *M_PI/180);  
+    s = sin(angle *M_PI/180);  
+    
+    x1 = floor(x1 * c + y1 * s);  
+    y1 = floor(-x1 * s + y1 * c);  
+    x2 = floor(x2 * c + y2 * s);  
+    y2 = floor(-x2 * s + y2 * c);  
+    x3 = floor(x3 * c + y3 * s);  
+    y3 = floor(-x3 * s + y3 * c);  
+    
+    cleardevice();  
+    line(x1, y1 ,x2, y2);  
+    line(x2,y2, x3,y3);  
+    line(x3, y3, x1, y1);  
+    getch();  
+    closegraph();  
+    return 0;  
+}  
